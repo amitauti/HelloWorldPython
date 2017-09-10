454c672b98365a8c2fba644c074d4632b246ae89
amitauti <amitauti@users.noreply.github.com>
2017-09-11 00:15:11 +0530

    ex01
    added solution

---
+++ CharacterInput.py
@@ -0,0 +1,16 @@
+# This is tutorial from http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+# Create a program that asks the user to enter their name and their age. Print out a message addressed
+#  to them that tells them the year that they will turn 100 years old.
+
+from datetime import date
+
+def main():
+    name = get_name()
+    age = get_age()
+    bday_yet = birtday_yet()
+    year = get_year(int(age), bday_yet)
+
+    if year == 'greater':
+        print("You are already 100 years old")
+    else:
+        print("Hello" )
