b57ecaafbdee2f8628a6a79675a8affb55d6fa2a
amitauti <amitauti@users.noreply.github.com>
2017-09-10 12:26:42 +0530

    initial commit
    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,72 @@
+# Tut -1
+# print ("Hello World")
+# Tut -2
+# x = 1
+# if x == 1:
+#     #see the indentation
+#     print("x = 1.")
+
+# Tut-3
+# print ("Good bye world")
+
+# Tut-4
+# myint = 7
+# print (myint)
+
+# Tut-5
+# myfloat = 7.0
+# print(myfloat)
+# myfloat = float(7)
+# print(myfloat)
+
+# Tut-7
+# mystring = 'hello'
+# print(mystring)
+#
+# mystring = "hello"
+# print(mystring)
+
+
+# Tut-8
+# mystring = "Don't worry about the quotes"
+# print(mystring)
+
+# Tut-9
+# one = 1
+# two = 2
+# three = one + two
+# print(three)
+#
+#
+#
+# hello = "hello"
+# world = "world"
+#
+# hello_world = hello + " " + world
+# print(hello_world)
+
+# tut=10
+# a , b = 3 ,4
+# print(a,b)
+
+# # tut-11
+# one = 1
+# two = 2
+# hello = "hello"
+# print(one + two + hello)
+
+# tut-12
+# mystring = "hello"
+# myfloat = 10.0
+# myint = 20
+#
+# if mystring == "hello":
+#     print("String: %s" % mystring)
+#
+# if isinstance(myfloat, float) and myfloat == 10.0:
+#     print("Float: %f" % myfloat)
+#
+# if isinstance(myint, int) and myint == 20:
+#     print("Int: %d" % myint)
+#
+# tut-13
